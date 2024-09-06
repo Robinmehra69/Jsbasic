0a52e09f75d1d9f04b421ab060676237a7e19a18
Robinmehra69 <109615057+Robinmehra69@users.noreply.github.com>
2024-09-06 06:20:45 +0000

    datatypes and variable done

---
+++ basics/dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert (3+3) // we are using node js, not browser
+console.log(3 + 4);
+console.log(5 + 5);
+
+let name  ="hitesh"
+let age = 18
+let isLoggedIn = false
+
+//number =>2 to the power of 53
+//bigint for big number
+//string => ""
+//boolean =>true/false
+//null -stand alone value
+//undefined=> not declared
+//symbol => to find uiqueness
+
+//object 
+
+
+console.log(typeof null)  //=> object 
+
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accc= 1134
+let acccount = "hitesh@gmail.com"
+var acc= "234" //don't use it ,has scope problem in block scope and functional scope
+ acc= "jaipur"
+
+ console.table([acc,acccount,accc])
+ console.log(acc)
+ 
