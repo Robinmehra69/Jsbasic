8727bb2f4a19699daa400a703b837e71fb076bb9
Robinmehra69 <109615057+Robinmehra69@users.noreply.github.com>
2024-09-07 06:44:46 +0000

    datatype basics and comparision

--- basics/dataType.js
+++ basics/dataType.js
@@ -4,9 +4,11 @@
 console.log(3 + 4);
 console.log(5 + 5);
 
-let name  ="hitesh"
-let age = 18
-let isLoggedIn = false
+// let name  ="hitesh"
+// let age = 18
+// let isLoggedIn = false
+
+//****primitive data types - call by value send the copy of value
 
 //number =>2 to the power of 53
 //bigint for big number
@@ -16,8 +18,21 @@ let isLoggedIn = false
 //undefined=> not declared
 //symbol => to find uiqueness
 
-//object 
+//*******reference -(non-primitive)*/
 
+//objects,array,functions
+//master js objects and browser events to become prooo!
 
-console.log(typeof null)  //=> object 
+// console.log(typeof null)  //=> object 
+// const isLog= false
+// let userEmail;
 
+const id =Symbol('1123')
+const id1 = Symbol('1123')
+console.log(id===id1)
+const bigNumber = 2334535325n
+console.log(bigNumber)
+
+//null datatype - Object
+//undefined - undefined
+//function - object function
